18eb3c3e3fb52eb340581d15ba36a8c3a499e435
Jeff Moroski <jmoroski@vmware.com>
2019-01-04 12:17:10 -0500

    Move GPG expanstion to deploy script.
    Secure environment variables aren't available in pull requests from forks.
    Running these commands on every build breaks the dewvelopment process.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- build-resources/travis-release.sh
+++ build-resources/travis-release.sh
@@ -5,6 +5,9 @@
 #
 
 # Prerun the script to download all the dependent artifacts
+openssl aes-256-cbc -K $encrypted_fd599ca5bd58_key -iv $encrypted_fd599ca5bd58_iv -in build-resources/gpg_files.tar.enc -out build-resources/gpg_files.tar -d
+tar -f build-resources/gpg_files.tar -xO gpg-secret-keys | $GPG_EXECUTABLE --import
+tar -f build-resources/gpg_files.tar -xO gpg-ownertrust | $GPG_EXECUTABLE --import-ownertrust
 mvn help:evaluate -Dexpression=project.version
 
 current_pom_version=`mvn help:evaluate -Dexpression=project.version | grep -v '^\['`
