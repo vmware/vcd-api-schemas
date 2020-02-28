045e72648108c1d3bbac3c135f427fc026954b6f
Steve Jones <sjones@vmware.com>
2020-02-28 11:23:31 -0500

    Update encryption for gpg signing files
    Signed-off-by: Steve Jones <sjones@vmware.com>

--- build-resources/travis-release.sh
+++ build-resources/travis-release.sh
@@ -5,7 +5,7 @@
 #
 
 # Prerun the script to download all the dependent artifacts
-openssl aes-256-cbc -K $encrypted_fd599ca5bd58_key -iv $encrypted_fd599ca5bd58_iv -in build-resources/gpg_files.tar.enc -out build-resources/gpg_files.tar -d
+openssl aes-256-cbc -K $encrypted_c59801cc017b_key -iv $encrypted_c59801cc017b_iv -in build-resources/gpg_files.tar.enc -out build-resources/gpg_files.tar -d
 tar -f build-resources/gpg_files.tar -xO gpg-secret-keys | $GPG_EXECUTABLE --import
 tar -f build-resources/gpg_files.tar -xO gpg-ownertrust | $GPG_EXECUTABLE --import-ownertrust
 mvn help:evaluate -Dexpression=project.version
