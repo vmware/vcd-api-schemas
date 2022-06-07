ed7723bcc8c198f641c090106bff7639f6dd233f
abarreiro <abarreiro@vmware.com>
2022-06-07 09:53:18 +0200

    Remove node_modules
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- bindings/typescript/api-bindings/node_modules/cpy/cpy-error.js
+++
@@ -1,9 +0,0 @@
-import NestedError from 'nested-error-stacks';
-
-export default class CpyError extends NestedError {
-	constructor(message, nested) {
-		super(message, nested);
-		Object.assign(this, nested);
-		this.name = 'CpyError';
-	}
-}
